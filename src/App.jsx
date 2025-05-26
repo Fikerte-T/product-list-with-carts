d09b5d1fc114ec5623dcaff453a413c8bfcd7059
fikerte <tofikertesfaye@gmail.com>
2025-05-26 22:35:32 +0300

    use vite's dynamic import

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ function App() {
   }, [cartItems])
   
   useEffect(() => {
-    fetch('./data.json')
+    fetch(`${import.meta.env.BASE_URL}data.json`)
     .then(res => res.json())
     .then(data => {
       const updatedData = data.map(prod => (
